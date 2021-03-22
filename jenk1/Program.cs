eb2bda2eda40acd649961a43bbc42ac6b3ee0f2d
rheajo <rheadsouza356@gmail.com>
2021-03-22 14:41:49 +0530

    Add project files.

---
+++ jenk1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenk1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 23;
+            Console.WriteLine(b);
+        }
+    }
+}
